829c9de0637825c71bd810d746aba63721ea60df
NKulling <78426848+NKulling@users.noreply.github.com>
2023-02-10 10:01:47 +0100

    Update Visibility_index.R

--- Visibility_index.R
+++ Visibility_index.R
@@ -10,10 +10,8 @@ library(pbmcapply)
 #Local variables
 
 setwd("...")
-datafold<-paste(getwd(),"/data",sep="")
-results<-paste(getwd(),"/results",sep="")
-
-ncores<-16 # number of cores used
+datafold<-paste(getwd(),"/data",sep="")    #folder containing points shapefile
+results<-paste(getwd(),"/results",sep="")  #output folder
 
 #-- digital surface model (from swissSurface, aggregated at 25m)
 dsm<-rast(".../surfaceheight.tif")
@@ -25,12 +23,17 @@ crs(dtm)<-crs(dsm)
 print("all variables loaded")
 
 #-- Study area grid points
-
 points<-st_read(paste(datafold,"25m_grid_gph.shp",sep="/"))
 points$obs<-NA # This column will get filled with amount of cells visible per pixel
 points<-st_transform(points,crs(dtm))
 
 print("points crs transformed")
+
+#-- Visibility function parameters
+
+ncores<-16          # number of cores used
+obs_height<-1.7     # observer height
+max_dist<-16000     # maximum visibility distance (in meters)
 
 #-- Running viewshed analysis
 
@@ -44,8 +47,8 @@ vis<-function(x){
     p1,
     dsm,
     dtm,
-    max_distance = 16000,
-    observer_height = 1.7,
+    max_distance = max_dist,
+    observer_height = obs_height,
   )
   v<-table(values(a))[2]  
   return(v)
@@ -61,7 +64,7 @@ points$obs<-vv
 
 points_obs<-vect(points)
 
-writeVector(points_obs, paste(results,"points_viewshed_25m.shp",sep="/"))
+writeVector(points_obs, paste(results,"points_viewshed.shp",sep="/"))
 
 end<-Sys.time()
 elapsed<-difftime(end,st)
